a7c658dedd4cdf0d7ae4f4cdd646b3fb599292cc
Mateus Silva <mateuss_gt@hotmail.com>
2023-07-02 19:09:03 -0300

    Novos testes de caso de uso GetCastUsecase e ajustes de dependencia

--- test/app/features/home/presentation/stores/movie_details_store_test.dart
+++ test/app/features/home/presentation/stores/movie_details_store_test.dart
@@ -4,21 +4,29 @@ import 'package:mocktail/mocktail.dart';
 import 'package:o_que_assistir/app/core/error/failure.dart';
 import 'package:o_que_assistir/app/core/error/failure_extension.dart';
 import 'package:o_que_assistir/app/features/home/data/models/gender_model.dart';
+import 'package:o_que_assistir/app/features/home/domain/entities/actor_entity.dart';
 import 'package:o_que_assistir/app/features/home/domain/entities/movie_entity.dart';
+import 'package:o_que_assistir/app/features/home/domain/usecases/get_cast_usecase.dart';
 import 'package:o_que_assistir/app/features/home/domain/usecases/get_movie_usecase.dart';
 import 'package:o_que_assistir/app/features/home/presentation/stores/movie_details_store.dart';
 
 class MockGetMovieUsecase extends Mock implements GetMovieUsecase {}
 
+class MockGetCastUsecase extends Mock implements GetCastUsecase {}
+
 void main() {
   late MovieDetailsStore store;
   late MockGetMovieUsecase mockGetMovieUsecase;
+  late MockGetCastUsecase mockGetCastUsecase;
+
   const int tMovieId = 550;
 
   setUp(() {
     mockGetMovieUsecase = MockGetMovieUsecase();
-    store = MovieDetailsStore(mockGetMovieUsecase);
+    mockGetCastUsecase = MockGetCastUsecase();
+    store = MovieDetailsStore(mockGetMovieUsecase, mockGetCastUsecase);
     registerFallbackValue(GetMovieParams(tMovieId));
+    registerFallbackValue(GetCastParams(tMovieId));
   });
 
   const tId = "550";
@@ -37,7 +45,8 @@ void main() {
   const tVoteAverage = 8.435;
   const tVoteCount = 26651;
   const tRuntime = 139;
-  const tOverview = '''
+  const tOverview =
+      '''
 A ticking-time-bomb insomniac and a slippery soap salesman channel primal male 
 aggression into a shocking new form of therapy. Their concept catches on, with 
 underground "fight clubs" forming in every town, until an eccentric gets in the 
@@ -59,10 +68,38 @@ way and ignites an out-of-control spiral toward oblivion.''';
     runtime: tRuntime,
   );
 
-  test('should get data from use case', () async {
+  final tCast = <ActorEntity>[
+    const ActorEntity(
+      id: "819",
+      name: 'Edward Norton',
+      character: 'The Narrator',
+      profilePath: '/eIkFHNlfretLS1spAcIoihKUS62.jpg',
+      knownFor: "Acting",
+      popularity: 1,
+    ),
+    const ActorEntity(
+      id: "287",
+      name: 'Brad Pitt',
+      character: 'Tyler Durden',
+      profilePath: '/kc3M04QQAuZ9woUvH3Ju5T7ZqG5.jpg',
+      knownFor: "Acting",
+      popularity: 1,
+    ),
+    const ActorEntity(
+      id: "1283",
+      name: 'Helena Bonham Carter',
+      character: 'Marla Singer',
+      profilePath: '/58oJPFG1wefMC0Vj7sUzbzbmEiz.jpg',
+      knownFor: "Acting",
+      popularity: 1,
+    ),
+  ];
+
+  test('should get data from get movie usecase', () async {
     // arrange
     when(() => mockGetMovieUsecase(any()))
         .thenAnswer((_) async => Right(tMovie));
+    when(() => mockGetCastUsecase(any())).thenAnswer((_) async => Right(tCast));
 
     // act
     await store.getMovie(tMovieId);
@@ -73,10 +110,11 @@ way and ignites an out-of-control spiral toward oblivion.''';
     verifyNoMoreInteractions(mockGetMovieUsecase);
   });
 
-  test('should get error from use case', () async {
+  test('should get error from get movie usecase', () async {
     // arrange
     when(() => mockGetMovieUsecase(any()))
         .thenAnswer((_) async => Left(ServerFailure()));
+    when(() => mockGetCastUsecase(any())).thenAnswer((_) async => Right(tCast));
 
     // act
     await store.getMovie(tMovieId);
@@ -85,5 +123,36 @@ way and ignites an out-of-control spiral toward oblivion.''';
     expect(store.errorMessageStream.value, ServerFailure().message);
     verify(() => mockGetMovieUsecase(GetMovieParams(tMovieId)));
     verifyNoMoreInteractions(mockGetMovieUsecase);
+  });
+
+  test("should get cast data from usecase", () async {
+    //arrange
+    when(() => mockGetMovieUsecase(any()))
+        .thenAnswer((_) async => Right(tMovie));
+    when(() => mockGetCastUsecase(any())).thenAnswer((_) async => Right(tCast));
+
+    //act
+    await store.getMovie(tMovieId);
+
+    //assert
+    expect(store.cast, tCast);
+    verify(() => mockGetCastUsecase(GetCastParams(tMovieId)));
+    verifyNoMoreInteractions(mockGetCastUsecase);
+  });
+
+  test("should get error from get cast usecase", () async {
+    //arrange
+    when(() => mockGetCastUsecase(any()))
+        .thenAnswer((_) async => Left(ServerFailure()));
+    when(() => mockGetMovieUsecase(any()))
+        .thenAnswer((_) async => Right(tMovie));
+
+    //act
+    await store.getMovie(tMovieId);
+
+    //assert
+    expect(store.errorMessageStream.value, ServerFailure().message);
+    verify(() => mockGetCastUsecase(GetCastParams(tMovieId)));
+    verifyNoMoreInteractions(mockGetCastUsecase);
   });
 }
